69d1aac8d19d173483bab0ae4d739a95bd7c8522
Hunter Parks <hunter@prks.cc>
2022-01-22 13:12:13 -0600

    Update to simplify and comply with guidelines

--- scripts/background.js
+++ scripts/background.js
@@ -1,43 +1,72 @@
-function showAlert(message) {
-    alert(message);
+async function alertUser({
+    html,
+    title = chrome.runtime.getManifest().name,
+    width = 400,
+    height = 250,
+    left,
+    top
+}) {
+    const window = left == null && top == null && await chrome.windows.getCurrent();
+    const window2 = await chrome.windows.create({
+        url: `data:text/html,<title>${title}</title>${html}`.replace(/#/g, '%23'),
+        type: 'popup',
+        left: left ?? Math.floor(window.left + (window.width - width) / 2),
+        top: top ?? Math.floor(window.top + (window.height - height) / 2),
+        height,
+        width
+    });
+    return new Promise((resolve) => {
+        chrome.windows.onRemoved.addListener(onRemoved, { windowTypes: [ 'popup' ] });
+        function onRemoved(id) {
+            if (id === window2.id) {
+                chrome.windows.onRemoved.removeListener(onRemoved);
+                resolve();
+            }
+        }
+    });
 }
+
+const divStyle = `style="text-align:center;font-family:sans-serif;padding-top:0.5rem;`;
+const inputHtml = `<input
+    style="padding:0.5rem 1.25rem; border:unset; border-radius:1.5rem;"
+    type="button"
+    onclick="window.close()"
+    value="Close"
+/>`;
 
 chrome.action.onClicked.addListener(
     (tab) => {
         let tabUrl = tab.url;
         if (tabUrl.indexOf('chrome.google.com/webstore') >= 0) {
-            const message = `This extension will not run on the Chrome Web Store.
-Please try on another tab.
-DON'T LET YOUR DREAMS BE DREAMS!
-DO IT!!!`;
-            chrome.scripting.executeScript({
-                target: { tabId: tab.id },
-                func: showAlert(message)
-            });
+            alertUser({ html: `<div ${divStyle}">
+    <p>This extension will not run on the<br/>Chrome Web Store.<br/>
+    Please try on another tab.</p>
+    <p>DON'T LET YOUR DREAMS BE DREAMS!<br/>
+    DO IT!!!</p>
+    ${inputHtml}
+</div>` })
+                .then(() => {
+                    console.log('Chrome Web Store alert closed.');
+                });
         } else if (tabUrl.indexOf("chrome://") >= 0) {
-            const message = `This extension will not run on this website.
-Please try on another tab.
-WHAT ARE YOU WAITING FOR?
-DO IT!!!`;
-            chrome.scripting.executeScript({
-                target: { tabId: tab.id },
-                func: showAlert(message)
-            });
+            alertUser({ html: `<div ${divStyle}">
+    <p>This extension will not run on this page.<br/>
+    Please try on another tab.</p>
+    <p>WHAT ARE YOU WAITING FOR?<br/>
+    DO IT!!!</p><br/>
+    ${inputHtml}
+</div>` })
+                .then(() => {
+                    console.log('Chrome tab alert closed.');
+                });
+        } else {
+            chrome.scripting.executeScript(
+                {
+                    target: { tabId: tab.id },
+                    files: [ 'scripts/doit.js' ]
+                },
+                null
+            );
         }
-        chrome.scripting.executeScript(
-            {
-                target: { tabId: tab.id },
-                files: [ 'scripts/jquery.min.js' ]
-            },
-            () => {
-                chrome.scripting.executeScript(
-                    {
-                        target: { tabId: tab.id },
-                        files: [ 'scripts/doit.js' ]
-                    },
-                    null
-                );
-            }
-        );
     }
 );
--- scripts/doit.js
+++ scripts/doit.js
@@ -7,37 +7,41 @@ function injectCss() {
 }
 
 function removeVideo() {
-    var $videoElement = $('.shia-do-it');
-    if ($videoElement !== null) $videoElement.remove();
+    const videoElement = document.getElementsByClassName('shia-do-it')[0];
+    if (videoElement) videoElement.remove();
 }
 
 function addVideo() {
     removeVideo();
 
-    var $videoDiv = $.parseHTML('<div class="shia-do-it"><div class="container"><video width="960" height="540" name="media"></div></div>');
-    $('body').append($videoDiv);
+    const videoFilename = `assets/${Math.floor(Math.random() * (6 - 1)) + 1}.webm`;
+    const videoUrl = chrome.runtime.getURL(videoFilename);
 
-    var video = $($videoDiv).find('video').get(0);
-    var videoNum = Math.floor(Math.random() * (6 - 1)) + 1;
-    var filename = 'assets/' + videoNum + '.webm';
-    video.src = chrome.runtime.getURL(filename);
-
-    video.onended = function () {
-        removeVideo();
-    };
-
-    video.addEventListener('loadeddata', function () {
-        $(video).css('visibility', 'visible');
+    const video = document.createElement('video');
+    video.setAttribute('width', '960');
+    video.setAttribute('height', '540');
+    video.setAttribute('name', 'media');
+    video.setAttribute('src', videoUrl);
+    video.onended = () => removeVideo();
+    video.addEventListener('loadeddata', () => {
+        video.style.visibility = 'visible';
         video.play();
     }, false);
-
-
     video.onerror = function () {
         alert('ooops... Shia had a problem. try on another tab');
-        removeVideo(false);
+        removeVideo();
     };
-
     video.load();
+
+    const videoInnerDiv = document.createElement('div');
+    videoInnerDiv.classList.add('container');
+    videoInnerDiv.appendChild(video);
+
+    const videoDiv = document.createElement('div');
+    videoDiv.classList.add('shia-do-it');
+    videoDiv.appendChild(videoInnerDiv);
+
+    document.querySelector('body').appendChild(videoDiv);
 }
 
 (() => {
